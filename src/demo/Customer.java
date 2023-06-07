63440054d1e04265e642d783b861a89790972aef
ahussain <ahussain@sadmins-MacBook-Air.local>
2023-06-07 16:38:11 +0530

    thirsd commit

---
+++ src/demo/Customer.java
@@ -0,0 +1,24 @@
+package demo;
+
+public class Customer {
+
+	private int acountId;
+	private String name;
+
+	public int getAcountId() {
+		return acountId;
+	}
+
+	public void setAcountId(int acountId) {
+		this.acountId = acountId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
